f7c708a9b2b90478d3c86e1234c899e73ef0e86f
Destrifer <destrifer@yandex.ru>
2024-11-29 14:09:13 +0300

    try

--- templates/tariff-sync.php
+++ templates/tariff-sync.php
@@ -216,14 +216,29 @@ function getServicesHTML()
 {
     global $db;
     $html = '<div class="service-col">';
-    $sql = mysqli_query($db, 'SELECT r.`user_id`, r.`name` FROM `requests` r LEFT JOIN `users` u ON u.`id` = r.`user_id` WHERE r.`mod` = 1 AND u.`role_id` = 3 AND u.`status_id` = 1 ORDER BY r.`name`;');
+
+    // Запрос для получения сервисов с тарифами
+    $sql = mysqli_query($db, '
+        SELECT r.`user_id`, r.`name`, r.`tariff_id` 
+        FROM `requests` r 
+        LEFT JOIN `users` u ON u.`id` = r.`user_id` 
+        WHERE r.`mod` = 1 AND u.`role_id` = 3 AND u.`status_id` = 1 
+        ORDER BY r.`name`;
+    ');
+
     $n = 0;
     while ($row = mysqli_fetch_assoc($sql)) {
         if ($n == 12) {
             $html .= '</div><div class="service-col">';
             $n = 0;
         }
-        $html .= '<label class="service-row"><input data-check-flag="" type="checkbox" name="service_id[]" value="' . $row['user_id'] . '"> ' . $row['name'] . '</label>';
+
+        // Выводим имя сервиса и тариф
+        $html .= '<label class="service-row">
+                    <input data-check-flag="" type="checkbox" name="service_id[]" value="' . $row['user_id'] . '"> 
+                    ' . $row['name'] . '
+                    <br><small>Тариф: ' . ($row['tariff_id'] ? $row['tariff_id'] : 'Не указан') . '</small>
+                  </label>';
         $n++;
     }
     $html .= '</div>';
