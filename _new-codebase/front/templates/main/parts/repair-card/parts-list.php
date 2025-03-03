c56fab68a9feb93a6b281411340f45828c025f61
Destrifer <destrifer@yandex.ru>
2025-03-03 12:27:19 +0300

    fix

--- _new-codebase/front/templates/main/parts/repair-card/parts-list.php
+++ _new-codebase/front/templates/main/parts/repair-card/parts-list.php
@@ -190,16 +190,14 @@ function old(array $groups, array $providers, array $cats, $userRole = '', $hasS
             </div>';
 }
 
-
-function getPartsListHTML(array $parts)
+function getPartsListHTML(array $parts, $userRole = '')
 {
     ob_start();
-    echo getPartsListItemsHTML($parts);
+    echo getPartsListItemsHTML($parts, $userRole);
     return ob_get_clean();
 }
 
-
-function getPartsListItemsHTML(array $parts)
+function getPartsListItemsHTML(array $parts, $userRole = '')
 {
     ob_start();
     echo '<div class="row">';
@@ -207,15 +205,24 @@ function getPartsListItemsHTML(array $parts)
         echo '<div class="col-12"><p style="text-align: center;padding: 32px 0">Запчасти отсутствуют.</p></div>';
     } else {
         foreach ($parts as $part) {
-            echo '<div data-part class="col-12 col-sm-6" style="padding-bottom: 32px;" data-has-original-flag="' . ((!empty($part['has_original_flag'])) ? '1' : '0') . '" data-attr-id="' . $part['attr_id'] . '" data-type-id="' . $part['type_id'] . '" data-group-id="' . $part['group_id'] . '" data-origin="store" data-id="' . $part['id'] . '">
-            <div class="parts-list__item ' . ((!empty($part['has_original_flag'])) ? 'parts-list__item_secondary' : '') . '">';
+            echo '<div data-part class="col-12 col-sm-6" style="padding-bottom: 32px;" 
+                data-has-original-flag="' . ((!empty($part['has_original_flag'])) ? '1' : '0') . '" 
+                data-attr-id="' . ($part['attr_id'] ?? '') . '" 
+                data-type-id="' . ($part['type_id'] ?? '') . '" 
+                data-group-id="' . ($part['group_id'] ?? '') . '" 
+                data-origin="store" 
+                data-id="' . ($part['id'] ?? '') . '">
+                
+                <div class="parts-list__item ' . ((!empty($part['has_original_flag'])) ? 'parts-list__item_secondary' : '') . '">';
+            
             mainCol($part);
             photosCol($part['photos']);
-            if ($userRole != 'master'):
-							controlsCol();
-						endif;
-            echo '</div>
-            </div>';
+
+            if (isset($userRole) && in_array($userRole, ['admin', 'store', 'slave-admin'])) {
+                controlsCol();
+            }
+
+            echo '</div></div>';
         }
     }
     echo '</div>';
