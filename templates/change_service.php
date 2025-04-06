e42239de716fd3c3ed4f99c9f4543d146930274b
Destrifer <destrifer@yandex.ru>
2025-04-06 18:40:02 +0300

    fix

--- templates/change_service.php
+++ templates/change_service.php
@@ -1,4 +1,41 @@
 
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'].'/_new-codebase/config.php';
+
+function getRepairsWithServices($db) {
+    $repairs = [];
+    $sql = mysqli_query($db, "SELECT r.id, r.title, r.service_id, s.name AS service_name 
+                              FROM repairs r 
+                              LEFT JOIN requests s ON r.service_id = s.id 
+                              ORDER BY r.id DESC LIMIT 100");
+
+    while ($row = mysqli_fetch_assoc($sql)) {
+        $repairs[] = $row;
+    }
+    return $repairs;
+}
+
+function getAllServices($db) {
+    $services = [];
+    $sql = mysqli_query($db, "SELECT id, name FROM requests ORDER BY name");
+    while ($row = mysqli_fetch_assoc($sql)) {
+        $services[] = $row;
+    }
+    return $services;
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['repair_id'], $_POST['new_service_id'])) {
+    $repairId = intval($_POST['repair_id']);
+    $newServiceId = intval($_POST['new_service_id']);
+    mysqli_query($db, "UPDATE repairs SET service_id = '{$newServiceId}' WHERE id = '{$repairId}'");
+    header("Location: /change_service.php?success=1");
+    exit;
+}
+
+$repairs = getRepairsWithServices($db);
+$services = getAllServices($db);
+?>
+
 <!doctype html>
 <html>
 <head>
