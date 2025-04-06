92cf4291b7ec67f4d1bb9e7b0cba9413b59cb963
Destrifer <destrifer@yandex.ru>
2025-04-06 18:42:16 +0300

    fix

--- templates/change_service.php
+++ templates/change_service.php
@@ -10,9 +10,6 @@
   <title>Смена сервиса у ремонта</title>
   <link href="/css/style.css" rel="stylesheet">
   <link rel="stylesheet" type="text/css" href="/css/datatables.css">
-  <script src="/_new-codebase/front/vendor/jquery/jquery-1.7.2.min.js"></script>
-  <script src="/_new-codebase/front/vendor/datatables/1.10.12/jquery.dataTables.min.js"></script>
-  <script>
 
 </head>
 <body>
