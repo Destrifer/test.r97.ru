e7a8e4aa7132be9f582d1f6aaea0eac7b5e26924
Destrifer <destrifer@yandex.ru>
2025-04-15 17:03:32 +0300

    try

--- templates/change_service_server.php
+++ templates/change_service_server.php
@@ -1,5 +1,7 @@
 
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 require_once $_SERVER['DOCUMENT_ROOT'].'/_new-codebase/config.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/includes/configuration.php';
 
